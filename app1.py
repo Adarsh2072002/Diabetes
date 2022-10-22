87ddf5db07249ef7a778b3ffea01df23bdf68410
angadisumanth10 <91518618+angadisumanth10@users.noreply.github.com>
2022-10-22 23:07:32 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template,url_for,request
+import pickle
+import numpy as np
+app = Flask(__name__)
+
+model = pickle.load(open('diabetes.pkl','rb'))
+@app.route("/")
+def home():
+    return render_template('ind1.html') 
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    float_features =[float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    model = pickle.load(open('diabetes.pkl','rb'))
+    pred = model.predict(features)
+    if pred==1:
+        strr = "You have Diabetes :("
+    else:
+        strr = "You are healthy :)"
+    
+    return render_template("ind1.html", text_prediction ="Your Model Output is {} and {}".format(pred,strr)) 
+if __name__=='__main__':
+    app.run()
---
+++ diabetes.py
@@ -0,0 +1,23 @@
+
+
+import pandas as pd
+ 
+from sklearn.neighbors import KNeighborsClassifier       
+
+from sklearn.model_selection import train_test_split
+import pickle
+
+df=pd.read_csv('model.csv')
+print(df.head())
+
+x= df.iloc[:,:-1]
+y=df.iloc[:,-1]
+x_train,x_test,y_train,y_test= train_test_split(x,y, test_size = 0.1)
+
+model = KNeighborsClassifier(n_neighbors=3)
+
+model.fit(x_train,y_train)
+
+pred=model.predict([[90,5]])
+print(pred)
+pickle.dump(model,open('diabetes.pkl','wb'))
